3540a6738c73614a9375131853b8e1092ee39555
Barman Hrishikesh <hrishikesh.barman@volvo.com>
2021-08-25 23:31:01 +0530

    updated html and css

---
+++ app.js
@@ -0,0 +1,28 @@
+var inputBox = document.querySelector("#input-Box");
+var btnTranslate = document.querySelector(".btn-translate");
+var outputBox = document.querySelector(".output-Box")
+
+const url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// const url = "https://api.funtranslations.com/translate/pirate.json"
+
+
+var urlConverter = (inputBox) => {
+    return url + "?text=" + inputBox.value
+}
+
+function errorHandler(error){
+    console.log(error);
+}
+
+
+function fetchcall(){
+    fetch(urlConverter(inputBox))
+    .then(response => response.json())
+    .then(data => {
+        var out = data.contents.translated;
+        outputBox.innerText = out;
+    }  )
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", fetchcall)
